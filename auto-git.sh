1c32b0f4999b5ca51b80bbe9a5f6e3707b3836b1
João Marcos Corrêa <desenvolvimento9@softin.com.br>
2022-08-26 13:28:50 -0300

    shell momento

---
+++ auto-git.sh
@@ -0,0 +1,9 @@
+#! /bin/shell
+
+echo -e "What you want to do?\n"
+echo "[1] git fetch and pull" &&
+echo "[2] git commit" &&
+echo "[3] git commit and push" &&
+echo "[4] git push" &&
+echo -e "[5] new branch\n" &&
+echo -n "Enter: " ; read CHOICE
